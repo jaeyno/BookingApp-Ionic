494da57509708fea47205fe39a5c0cf9470b07ef
Jae No <jaeyno@gmail.com>
2021-08-17 19:32:35 -0400

    [Ionic-Angular] add recipe list

--- ionic-angular/src/app/recipes/recipes.page.ts
+++ ionic-angular/src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Apple Pie',
+      imageUrl: 'https://www.inspiredtaste.net/wp-content/uploads/2019/10/Homemade-Apple-Pie-Recipe-6-1200.jpg',
+      ingredients: ['Apple', 'Flour', 'Sugar', 'Honey']
+    },
+    {
+      id: 'r2',
+      title: 'Fried Rice',
+      imageUrl: 'https://therecipecritic.com/wp-content/uploads/2019/07/easy_fried_rice.jpg',
+      ingredients: ['Rice', 'Egg', 'Sugar', 'Salt']
+    }
+  ];
 
   constructor() { }
 
