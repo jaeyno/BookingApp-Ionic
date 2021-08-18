c264651b97535aa08a5bdcdcea47f7e899bf0ed2
Jae No <jaeyno@gmail.com>
2021-08-17 20:08:53 -0400

    [Ionic-Angular] extract and display route param

--- ionic-angular/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ ionic-angular/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,7 @@
+import { RecipesService } from './../recipes.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailPage implements OnInit {
 
-  constructor() { }
+  loadedRecipe: Recipe;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipesService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe( param => {
+      if (!param.has('recipeId')) {
+        //redirect to the detailed recipe page
+        return;
+      }
+      const recipeId = param.get('recipeId');
+      this.loadedRecipe = this.recipeService.getRecipe(recipeId);
+    });
   }
 
 }
