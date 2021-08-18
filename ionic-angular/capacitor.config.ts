af3812c1a5b16ebb21d7f5101e3cc40a430631ef
Jae No <jaeyno@gmail.com>
2021-08-18 18:45:03 -0400

    [ionic-angular] build android native app

--- ionic-angular/capacitor.config.ts
+++ ionic-angular/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ionicangular',
   appName: 'ionic-angular',
   webDir: 'www',
   bundledWebRuntime: false
