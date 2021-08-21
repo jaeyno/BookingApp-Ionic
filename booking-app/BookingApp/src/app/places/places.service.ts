00f52bc56d57c355f71e9e5576c77ae759309796
Jae No <jaeyno@gmail.com>
2021-08-20 20:58:59 -0400

    [Booking App] inject raw static data

--- booking-app/BookingApp/src/app/places/places.service.ts
+++ booking-app/BookingApp/src/app/places/places.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Place } from './place.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
+  private _places: Place[] = [
+    new Place('p1', 'Manhattan Mension', 'In the heart of New York City', 'https://aihkcdnstoragep01.blob.core.windows.net/pgl-release/Images/ArticleImages/19/19156.jpg', 149.99),
+    new Place('p2', 'Amour Toujoirs', 'A romantic place', 'https://media.tacdn.com/media/attractions-splice-spp-674x446/07/03/1c/9c.jpg', 200.21)
+  ];
 
   constructor() { }
+
+  get places() {
+    return [...this._places];
+  }
 }
