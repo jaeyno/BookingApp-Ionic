29bd03f66b45efc1c93061fecde3f94b866df107
Jae No <jaeyno@gmail.com>
2021-08-22 06:38:19 -0400

    [Booking App] fix the routing issue

--- booking-app/BookingApp/src/app/places/discover/discover-routing.module.ts
+++ booking-app/BookingApp/src/app/places/discover/discover-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     component: DiscoverPage
   },
   {
-    path: 'place-detail',
+    path: ':placeDetailId',
     loadChildren: () => import('./place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
   }
 ];
