8e311abeb050d1c0acae81af86bba612a7f06d9f
Jae No <jaeyno@gmail.com>
2021-08-22 12:56:43 -0400

    [Auth] create auth service

---
+++ booking-app/BookingApp/src/app/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private userIsAuthenticated = false;
+
+  get _userIsAuthenticated() {
+    return this.userIsAuthenticated;
+  }
+
+  constructor() { }
+
+  login() {
+    this.userIsAuthenticated = true;
+  }
+
+  logout() {
+    this.userIsAuthenticated = false;
+  }
+}
