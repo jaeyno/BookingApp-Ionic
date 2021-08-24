a4cde805bd40f620b5769be4ea57ce3e7a8c93f4
Jae No <jaeyno@gmail.com>
2021-08-24 08:38:20 -0400

    [ion-grid] css grid

--- booking-app/BookingApp/src/app/auth/auth.page.ts
+++ booking-app/BookingApp/src/app/auth/auth.page.ts
@@ -19,7 +19,7 @@ export class AuthPage implements OnInit {
 
   onLogin() {
     this.authService.login();
-    this.router.navigateByUrl('/places/discover');
+    this.router.navigateByUrl('/places');
   }
 
 }
